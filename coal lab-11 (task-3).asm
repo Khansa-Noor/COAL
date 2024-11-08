7c9e34e575321fc3ba78d04af597c77c419e5e30
Khansa-Noor <noorkhansa001@gmail.com>
2024-11-08 19:46:40 +0500

    Add files via upload

---
+++ coal lab-11 (task-3).asm
@@ -0,0 +1,52 @@
+.model small 
+.stack 100h
+.data 
+String db ?
+.code
+main proc
+    
+    mov ax,@data
+    mov dx,ax
+    mov si,offset String
+    
+    mov cx,4
+    
+    L1:
+    mov ah,1
+    int 21h
+    mov si, ax
+    
+   
+   push si
+     
+   loop L1
+    
+   mov dl,10
+   mov ah,2
+   int 21h
+   
+   mov dl,13
+   mov ah,2 
+   int 21h
+   
+    
+   mov cx,4
+   L2: 
+   pop dx
+    
+   mov ah,02
+   int 21h
+    
+   mov dl,10
+   mov ah,2
+   int 21h
+   
+   mov dl,13
+   mov ah,2 
+   int 21h
+   loop L2
+    
+mov ah,4ch
+int 21h  
+end main
+main endp
