16da3f126654dff43bf0556fb7a236dad977fa5b
Khansa-Noor <noorkhansa001@gmail.com>
2024-11-16 19:28:18 +0500

    Add files via upload

---
+++ coal lab-13 (task-1).asm
@@ -0,0 +1,35 @@
+.model small
+.stack 100h
+.data  
+.code
+main Proc
+
+    mov cx,5
+    
+ L1:
+   push cx
+   mov cx,3
+ L2: 
+ mov dl,'*'
+ mov ah,2
+ int 21h
+ 
+ loop L2
+ pop cx 
+ 
+    mov dl,10
+   mov ah,2
+   int 21h
+   
+   mov dl,13
+   mov ah,2 
+   int 21h
+ 
+ 
+ loop L1
+     
+     
+mov ah,4ch
+int 21h
+main endp
+end main
